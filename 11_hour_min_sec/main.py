1dbd1b5c44415cb7ccb556b2663b39dfd56e3ff6
Miclem <miclemabasie3@gmail.com>
2023-05-09 23:23:09 +0100

    Updated Hour Min Sec converter

--- 11_hour_min_sec/main.py
+++ 11_hour_min_sec/main.py
@@ -1,10 +1,16 @@
 def sec_conversion(total_seconds):
+    seconds = total_seconds  % 60
+    min = (total_seconds - seconds) % 3600
+    hour = (total_seconds - seconds - min)
+    sec = seconds
+    min = min // 60
+    hour = hour // 3600
     if hour == 0 and min == 0:
         return f"{sec}s"
     elif hour == 0 and sec == 0:
         return f"{min}m"
     elif min == 0 and sec == 0:
-        return f"{hour}h"
+        return f"{hour}hr"
     elif hour == 0:
         return f"{min}m {sec}s"
     elif min == 0:
